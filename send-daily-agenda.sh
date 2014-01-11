6870b1bcfc5753a51882e50914ebd4bfb3651f8e
Charles Macanka <cmacanka@gmail.com>
2014-01-11 13:56:10 -0500

    Added agenda script.
    Not the initial version, but the current one.

---
+++ send-daily-agenda.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+tmpfile=/tmp/tmpfile.$$
+emacs -batch -l /home/charles/.emacs.d/init.el -eval '(org-batch-agenda " ")' > $tmpfile
+# emacs -batch -l /home/charles/.emacs.d/init.el -eval '(org-batch-agenda " " org-agenda-files (quote ("~/Dropbox/Documents/org/todo.org")))' > /home/charles/Dropbox/Documents/org/agenda.txt
+
+current_time=`date`
+
+header=`echo "Daily agenda for $current_time:
+" | cat - $tmpfile`
+footer=`echo "Script used to create this email:" | cat - $0`
+#echo "Daily agenda for $current_time:
+#" | cat - Dropbox/Documents/org/agenda.txt | mail -s "Daily Agenda - $current_time" cmacanka@gmail.com
+
+echo "$header
+
+$footer" | mail -s "Daily Agenda - $current_time" cmacanka@gmail.com
